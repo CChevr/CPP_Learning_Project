832518c4de6ab0dfd7c6f2bc4c5beba6b837d2a4
CChevr <cedricus@msn.com>
2022-03-23 11:51:34 +0100

    first commit

---
+++ src/aircraft_manager.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "aircraft.hpp"
+
+class AircraftManager {
+    public:
+    void add(unique_ptr<Aircraft> aircraft) {
+        aircrafts.emplace_back(std::move(aircraft));
+        std::cout << "Ajouté!" << std::endl;
+    }
+
+    private:
+        std::vector<std::unique_ptr<Aircraft>> aircrafts;
+
+};
